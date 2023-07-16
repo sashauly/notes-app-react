d5de6f980039204d93402d52793aa64a45444cf8
sashauly <sashauly.code@gmail.com>
2023-07-16 20:46:31 +0300

    fix legacy react

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 import './style.css';
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.render(<App />, document.getElementById("root"))
