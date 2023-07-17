f2ae4c73613b88a6c01d39cb073faff0cd7ffcad
sashauly <sashauly.code@gmail.com>
2023-07-17 13:17:16 +0300

    update package, vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => ({
-  build: {
-    outDir: 'build',
-  },
-  base: '/notes-app-react/',
   plugins: [react()],
+  base: '/notes-app-react/',
 }));
